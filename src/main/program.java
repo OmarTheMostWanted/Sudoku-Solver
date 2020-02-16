60003bd9b78ad51a1d526d57fc20e315e1fbd079
othabet <o.thabet@student.tudelft.nl>
2020-02-16 22:15:55 +0100

    refactor

--- src/main/program.java
+++ src/main/program.java
@@ -1,9 +1,9 @@
 package main;
 
-public class main {
+public class program {
 
     public static void main(String[] args) {
-        SudokuBoard  sudokuBoard = new SudokuBoard("Medium 1");
+        SudokuBoard sudokuBoard = new SudokuBoard("Easy 1");
 //        System.out.println(sudokuBoard.toString());
         sudokuBoard.solve();
     }
